285c7d4ffe7cbf8bf9246974533e148d9419efe8
noobProgrammer35 <68321960+noobProgrammer35@users.noreply.github.com>
2020-08-13 19:14:05 +0800

    Delete Configuration.py

--- Configuration.py
+++
@@ -1,5 +0,0 @@
-vault_url = 'https://aspj.vault.azure.net/'
-tenant_id = 'ad854a3b-bf04-40c0-b761-8f25d9ba986d'
-client_id = '923b4b4b-35ce-4ea3-be7e-41c5a7360910'
-client_secret = "sBW9X4JY1jZ.0m2~6.s-~n5KjJM1dzV_Vw"
-InstrumentKey = 'InstrumentationKey=bec9fb90-0c7a-417a-809e-6c5417e4ba98'
