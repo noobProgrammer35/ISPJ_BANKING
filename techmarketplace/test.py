19cbf472f0968b0c8f408b1cf6f305f5d9edf963
Henry Boey <henryboey15@gmail.com>
2020-08-15 17:59:21 +0800

    production code

--- techmarketplace/test.py
+++ techmarketplace/test.py
@@ -1,4 +1,5 @@
 import redis
+import os
 from flask_kvsession import KVSessionExtension
 from flask_kvsession import SessionID
 from flask import current_app
@@ -12,8 +13,10 @@ from simplekv.decorator import PrefixDecorator
 # prefix_store = PrefixDecorator('session_',store)
 # s = KVSessionExtension(prefix_store,current_app)
 
-
-engine = create_engine('mysql+mysqlconnector://{0}:{1}@localhost/mydb'.format('dbmsuser','Henry123'))
+if os.environ.get('IS_PROD'):
+    engine = create_engine(os.environ.get('CLEARDB_DATABASE_URL'))
+else:
+    engine = create_engine('mysql+mysqlconnector://{0}:{1}@localhost/mydb'.format('dbmsuser','Henry123'))
 metadata = MetaData(bind=engine)
 store = SQLAlchemyStore(engine, metadata, 'session')
 # metadata.create_all()
