7a8fc910861fcb463e7edcb79c8363f634f7863b
Henry Boey <henryboey15@gmail.com>
2020-08-15 21:31:13 +0800

    ssl false tsl true

--- techmarketplace/config.py
+++ techmarketplace/config.py
@@ -19,7 +19,7 @@ def create_app():
         app.config['SECURITY_PASSWORD_SALT'] = os.urandom(16)
     app.config['UPLOAD_FOLDER'] = 'static\\upload'
     # email
-    app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USERNAME'] = 'dracula7723@gmail.com'
     app.config['MAIL_PASSWORD'] = 'hellfire67'
