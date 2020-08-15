5a1421453ac23d43b5ae0af580c99c70a5f0d54e
Henry Boey <henryboey15@gmail.com>
2020-08-15 17:15:48 +0800

    testing

--- techmarketplace/test.py
+++ techmarketplace/test.py
@@ -13,10 +13,10 @@ from simplekv.decorator import PrefixDecorator
 # s = KVSessionExtension(prefix_store,current_app)
 
 
-engine = create_engine('mysql+mysqlconnector://{0}:{1}@localhost/mydb'.format('dbmsuser','Henry123'))
-metadata = MetaData(bind=engine)
-store = SQLAlchemyStore(engine, metadata, 'session')
-# metadata.create_all()
-prefix_store = PrefixDecorator('session_',store)
-kvsession_extension = KVSessionExtension(prefix_store, current_app)
+# engine = create_engine('mysql+mysqlconnector://{0}:{1}@localhost/mydb'.format('dbmsuser','Henry123'))
+# metadata = MetaData(bind=engine)
+# store = SQLAlchemyStore(engine, metadata, 'session')
+# # metadata.create_all()
+# prefix_store = PrefixDecorator('session_',store)
+# kvsession_extension = KVSessionExtension(prefix_store, current_app)
 
