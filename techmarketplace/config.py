b1f8e28ef3c3413ccfb1027a1f8699b6ebc56d8e
Henry Boey <henryboey15@gmail.com>
2020-08-15 20:08:23 +0800

    production code

--- techmarketplace/config.py
+++ techmarketplace/config.py
@@ -22,7 +22,7 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USERNAME'] = 'piethonlee123@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'ASPJPYTHON123'
+    app.config['MAIL_PASSWORD'] = 'dracula123456789'
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
     app.config['WTF_CSRF_TIME_LIMIT'] = 3600
